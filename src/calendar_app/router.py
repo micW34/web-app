c0aea4cebedb8f33121eab7e9ad98590b78a5aeb
micW34 <108146278+micW34@users.noreply.github.com>
2023-11-07 13:42:05 -0600

    Chapter 2
    Created and connected HolidayService to endpoint.

--- src/calendar_app/router.py
+++ src/calendar_app/router.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from .services.holiday_service import HolidayService  # import class (service) w/ greeting method
 
 api = FastAPI()
 
 @api.get("/")
-def index():
-    return "Hello World"
+def hello(holiday_service = Depends(HolidayService)):
+    return holiday_service.get_hello("Micah")
---
+++ src/calendar_app/services/holiday_service.py
@@ -0,0 +1,4 @@
+class HolidayService:
+    def get_hello(self, name: str) -> str:
+        '''Given a name, create and return a greeting.'''
+        return f"Hello {name}"
